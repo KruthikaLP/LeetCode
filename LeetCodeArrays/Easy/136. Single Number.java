05aded2e0aaedfaf943f30fd3f13ff1b64b0f2e8
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2025-02-18 13:36:55 +0530

    Create 136. Single Number.java

---
+++ LeetCodeArrays/Easy/136. Single Number.java
@@ -0,0 +1,44 @@
+136. Single Number
+
+Hint
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+solution:=
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int n : nums){
+            if(map.containsKey(n)){
+                map.put(n,map.get(n)+1);
+            }
+            else{
+                map.put(n,1);
+            }
+        }
+        for(int n : nums){
+            if(map.get(n)==1){
+                return n;
+            }
+        }
+        return -1;
+    }
+}
