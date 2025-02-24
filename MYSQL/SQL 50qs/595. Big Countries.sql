f0a97e53d1d39382d9e1c0a1d1f8ff381cccad71
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2025-02-24 14:30:33 +0530

    Update 595. Big Countries.sql

--- MYSQL/SQL 50qs/595. Big Countries.sql
+++ MYSQL/SQL 50qs/595. Big Countries.sql
@@ -41,3 +41,9 @@ Output:
 | Afghanistan | 25500100   | 652230  |
 | Algeria     | 37100000   | 2381741 |
 +-------------+------------+---------+
+
+solutions:=
+# Write your MySQL query statement below
+ 
+select name,population,area from World  where area >=3000000 or 
+population >=25000000;
