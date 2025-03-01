0d16660e33be430ce3d7d4247fa8e545349d9b50
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2025-03-01 12:21:10 +0530

    Update 2460. Apply Operations to an Array.java

--- LeetCodeArrays/Two Pointers/Easy/2460. Apply Operations to an Array.java
+++ LeetCodeArrays/Two Pointers/Easy/2460. Apply Operations to an Array.java
@@ -38,7 +38,7 @@ class Solution {
         // Step 1: Modify the array by doubling adjacent equal elements
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] == nums[i + 1]) {
-                nums[i] *= 2;
+                nums[i] = nums[i]*2;
                 nums[i + 1] = 0;
             }
         }
@@ -49,7 +49,8 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
-                result[j++] = nums[i];
+                result[j] = nums[i];
+                j++;
             }
         }
 
